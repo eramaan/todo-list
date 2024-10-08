6d33751bb62ede5e2f1255fc328c064cce684c97
eramaan <8112194+eramaan@users.noreply.github.com>
2024-10-08 16:00:51 +0200

    added modal logic, modal view, select logic, minor fixes

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ function TodoList(id, title) {
 
 
 // populate some random stuff
-const bread = new TodoItem("0", "bread", "buy the bread", "25 oct", "2", "FALSE", "0")
-const homeworks = new TodoItem("1", "homeworks", "do the homeworks", "28 oct", "3", "FALSE", "1")
-const dog = new TodoItem("2", "walk the dog", "walk the werewolf", "10 oct", "1", "TRUE", "0")
+const bread = new TodoItem("0", "bread", "buy the bread", "2024-10-08", "2", false, 1)
+const homeworks = new TodoItem("1", "homeworks", "do the homeworks", "2024-10-16", "3", true, 1)
+const dog = new TodoItem("2", "walk the dog", "walk the werewolf", "2024-11-30", "1", true, 0)
 
 const work = new TodoList("0", "work hard")
 const play = new TodoList("1", "play harder")
@@ -53,7 +53,7 @@ function PopulateList() {
 
   todoItemsArray.forEach((item, index) => {
     const accordionTitle = document.createElement('button');
-    accordionTitle.textContent = `${index + 1} - ${item.title}, due for the ${item.dueDate} | priority ${item.priority}`;
+    accordionTitle.textContent = `${index + 1} - ${item.title}, due for ${item.dueDate} | priority ${item.priority}`;
     accordionTitle.classList.add('accordion');   
     
     listContainer.appendChild(accordionTitle);
@@ -61,12 +61,13 @@ function PopulateList() {
   
     const accordionBody = document.createElement('div');
     accordionBody.classList.add('panel');   
-    accordionBody.setAttribute('id',`${item.id}`);   
+    accordionBody.setAttribute('id',`${item.id}`);
+    accordionBody.setAttribute('querySelectorId',`item${item.id}`);   
+ 
   
     listContainer.appendChild(accordionBody);
   
-  
-      const panelContent = document.getElementById(`${item.id}`);
+      const panelContent = document.querySelector(`.panel[querySelectorId="item${item.id}"]`);
       const accordionP = document.createElement('p');
       accordionP.textContent = `${item.description}`;
       panelContent.appendChild(accordionP);
@@ -120,9 +121,9 @@ function PopulateListFromMenu(buttonId) {
   listContainer.innerHTML = "";
 
   todoItemsArray.forEach((item, index) => {
-    if (buttonId === item.idList) {
+    if (buttonId == item.idList) {
       const accordionTitle = document.createElement('button');
-      accordionTitle.textContent = `${index + 1} - ${item.title}, due for the ${item.dueDate} | priority ${item.priority}`;
+      accordionTitle.textContent = `${index + 1} - ${item.title}, due for ${item.dueDate} | priority ${item.priority}`;
       accordionTitle.classList.add('accordion');   
       
       listContainer.appendChild(accordionTitle);
@@ -141,7 +142,9 @@ function PopulateListFromMenu(buttonId) {
         const accordionP = document.createElement('p');
         accordionP.textContent = `${item.description}`;
         panelContent.appendChild(accordionP);
-    }  
+    } else {
+      //aggiungi cosa succede se la lista è vuota!
+    }
     });
 
     AccordionLogic();
@@ -159,38 +162,100 @@ btnNewList.addEventListener("click", function() {
   todoListsArray.push(newList);
   console.log(newList);
   PopulateMenu();
+  PopulateSelect();
 }
 )
 
 // create a new todo
 const btnNewTodo = document.getElementById("btn-new-todo")
 btnNewTodo.addEventListener("click", function() {
-  const newTodo = new TodoItem(todoItemsArray.length + 1, listTitle)
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
+  const dueDate = document.getElementById('dueDate').value;
+  const priority = document.getElementById('priority').value;
+  const done = document.getElementById('done').checked;
+  const idList = document.getElementById('idList').value;
+
+
+
+  const newTodo = new TodoItem(todoItemsArray.length + 1, title, description, dueDate, priority, done, idList)
+
+
   todoItemsArray.push(newTodo);
   console.log(newTodo);
+  
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+
   PopulateList();
-
-//sono arrivato qua, tocca fare il todo e salvare le info, con una modalità che riprendi per la modifica?
-
+  AccordionLogic();
 }
 )
 
-// accordion
-function AccordionLogic() {
-  let acc = document.getElementsByClassName("accordion");
-  let i;
-  
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      } 
-    });
-  }
+// populate the select
+function PopulateSelect() {
+  const selectElement = document.getElementById('idList');
+  selectElement.innerHTML = '';
+  todoListsArray.forEach(item => {
+    const option = document.createElement('option');
+    option.value = item.id;
+    option.textContent = item.title;
+    selectElement.appendChild(option); 
+  });
 }
 
-AccordionLogic();
+PopulateSelect();
+
+
+
+// accordion
+    function AccordionLogic() {
+      let acc = document.getElementsByClassName("accordion");
+      let i;
+      
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          } 
+        });
+      }
+    }
+
+    AccordionLogic();
+
+// modal logic
+
+    const modal = document.querySelector(".modal");
+    const overlay = document.querySelector(".overlay");
+    const openModalBtn = document.querySelector(".btn-open");
+    const closeModalBtn = document.querySelector(".btn-close");
+
+    // close modal function
+    const closeModal = function () {
+      modal.classList.add("hidden");
+      overlay.classList.add("hidden");
+    };
+
+    // close the modal when the close button and overlay is clicked
+    closeModalBtn.addEventListener("click", closeModal);
+    overlay.addEventListener("click", closeModal);
+
+    // close modal when the Esc key is pressed
+    document.addEventListener("keydown", function (e) {
+      if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+      }
+    });
+
+    // open modal function
+    const openModal = function () {
+      modal.classList.remove("hidden");
+      overlay.classList.remove("hidden");
+    };
+    // open modal event
+    openModalBtn.addEventListener("click", openModal);
